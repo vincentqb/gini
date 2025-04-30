a9753904cef5e50087376ccaf36b1bf6c6590f60
Vincent Quenneville-Belair <vincentqb@gmail.com>
2025-04-29 21:21:43 -0400

    fix name

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,7 +3,7 @@ import random
 import numpy as np
 import torch
 
-from gini import gini, gini_torch
+from gini import gini_numpy, gini_torch
 
 
 def test_example_1():
@@ -11,7 +11,7 @@ def test_example_1():
     array = np.zeros((1000))
     array[0] = 1.0
     score = 0.99890010998900103
-    assert np.abs(gini(array) - score).item() < 0.01
+    assert np.abs(gini_numpy(array) - score).item() < 0.01
 
     array = torch.from_numpy(array)
     assert torch.abs(gini_torch(array) - score).item() < 0.01
@@ -20,7 +20,7 @@ def test_example_1():
 def test_example_2():
     # For uniformly distributed random numbers, it will be low, around 0.33:
     array = np.random.uniform(-1, 0, 1000)
-    out = gini(array)
+    out = gini_numpy(array)
     assert (0 <= out).all() and (out <= 0.5).all()
 
     array = torch.from_numpy(array)
@@ -32,7 +32,7 @@ def test_example_3():
     # For a homogeneous sample, the Gini coefficient is 0.0:
     array = np.ones((1000))
     score = 0.0
-    assert np.abs(gini(array) - score).item() < 0.01
+    assert np.abs(gini_numpy(array) - score).item() < 0.01
 
     array = torch.from_numpy(array)
     assert torch.abs(gini_torch(array) - score).item() < 0.01
@@ -41,7 +41,7 @@ def test_example_3():
 def test_original_array_not_modified():
     array = np.random.uniform(-1, 0, 1000)
     array_ = array[:]
-    gini(array)
+    gini_numpy(array)
     assert (array == array_).all()
 
     array = torch.from_numpy(array)
@@ -55,11 +55,11 @@ def test_int():
     array = [1, 10, 100, 1_000, 10_000, 100_000, 1_000_000, 10_000_000, 100_000_000]
     array = np.array(array)
 
-    assert np.abs(gini(array) - score).item() < 0.01
+    assert np.abs(gini_numpy(array) - score).item() < 0.01
 
     array -= 1_000
 
-    assert np.abs(gini(array) - score).item() < 0.01
+    assert np.abs(gini_numpy(array) - score).item() < 0.01
 
     array = torch.from_numpy(array)
 
@@ -73,7 +73,7 @@ def test_int():
 def test_random_and_large():
     array = [random.randint(0, 10) for _ in range(100)] + [100_000_000]
     array = np.array(array)
-    assert gini(array).item() > 0.98
+    assert gini_numpy(array).item() > 0.98
 
     array = torch.from_numpy(array)
     assert gini_torch(array).item() > 0.98
@@ -84,11 +84,11 @@ def test_random_vector():
     array = [[random.randint(0, 10) for _ in range(shape[1])] for _ in range(shape[0])]
     array = np.array(array)
 
-    out = gini(array, axis=0)
+    out = gini_numpy(array, axis=0)
     assert out.shape[0] == shape[1]
     assert (0 <= out).all() and (out <= 1).all()
 
-    out = gini(array)
+    out = gini_numpy(array)
     assert out.shape[0] == shape[0]
     assert (0 <= out).all() and (out <= 1).all()
 
