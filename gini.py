c5c6420821b6ab3068219258f7857892f7a341a2
Vincent Quenneville-Belair <vincentqb@gmail.com>
2025-04-29 21:19:20 -0400

    torch implementation

--- gini.py
+++ gini.py
@@ -1,7 +1,4 @@
-import numpy as np
-
-
-def gini(array, axis=-1):
+def gini_numpy(array, axis=-1):
     """
     Calculate the Gini coefficient of a numpy array along an axis.
 
@@ -9,9 +6,10 @@ def gini(array, axis=-1):
     based on bottom equation: http://www.statsdirect.com/help/generatedimages/equations/equation154.svg
     from: http://www.statsdirect.com/help/default.htm#nonparametric_methods/gini.htm
     """
+    import numpy as np
 
     # Values must be strictly positive
-    array = array - np.amin(array) + 1e-7
+    array = array - np.min(array) + 1e-7
 
     # Values must be sorted
     array = np.sort(array, axis=axis)
@@ -26,3 +24,31 @@ def gini(array, axis=-1):
 
     # Gini coefficient
     return (np.sum((2 * index - n - 1) * array, axis=axis)) / (n * np.sum(array, axis=axis))
+
+
+def gini_torch(array, axis=-1):
+    """
+    Calculate the Gini coefficient of a numpy array along an axis.
+
+    vectorized implementation based on: https://github.com/oliviaguest/gini
+    based on bottom equation: http://www.statsdirect.com/help/generatedimages/equations/equation154.svg
+    from: http://www.statsdirect.com/help/default.htm#nonparametric_methods/gini.htm
+    """
+    import torch
+
+    # Values must be strictly positive
+    array = array - torch.min(array) + 1e-7
+
+    # Values must be sorted
+    array = torch.sort(array, dim=axis)[0]
+
+    # Index per array element
+    shape = [1 for _ in array.shape]
+    shape[axis] = -1
+    index = torch.arange(1, array.shape[axis] + 1, device=array.device).reshape(shape)
+
+    # Number of array elements
+    n = array.shape[axis]
+
+    # Gini coefficient
+    return (torch.sum((2 * index - n - 1) * array, axis=axis)) / (n * torch.sum(array, axis=axis))
